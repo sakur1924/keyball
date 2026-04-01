3810aab37d17c1441d7443126d31dd2d4938d1a7
sakur1924 <137590380+sakur1924@users.noreply.github.com>
2026-04-01 22:24:12 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_MINUS ,
     KC_Z     , KC_X     , KC_C     , KC_V     ,SFT_T(KC_B),                         SFT_T(KC_N), KC_M     , KC_COMMA , KC_DOT   , KC_SLASH ,
-    KC_LCTL  , KC_ALT   , KC_ESC   ,LT(2,KC_SPACE),CMD_T(KC_LNG2),LT(3,KC_ENT),    LT(4,KC_BSPC),LT(1,KC_LNG1),_______,_______,_______,KC_TAB
+    KC_LCTL  , KC_LALT   , KC_ESC   ,LT(2,KC_SPACE),CMD_T(KC_LNG2),LT(3,KC_ENT),    LT(4,KC_BSPC),LT(1,KC_LNG1),_______,_______,_______,KC_TAB
   ),
 
   [1] = LAYOUT_universal(
