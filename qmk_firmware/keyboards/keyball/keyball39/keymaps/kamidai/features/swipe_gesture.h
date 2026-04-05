dabf1052bf2b423aae8c4f2e315e808cd59201c7
sakur1924 <sakuraiyuta1234@gmail.com>
2026-04-05 18:58:47 +0900

    commit

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/features/swipe_gesture.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/kamidai/features/swipe_gesture.h
@@ -30,71 +30,7 @@ bool is_repeat = false;
 // スワイプジェスチャーで何が起こるかを処理する関数です
 // 上、下、左、右の4つのオプションがあります
 void process_swipe_gesture(int16_t x, int16_t y) {
-  // Alt
-  if (current_keycode == ALT_T(KC_F13)) {
-    unregister_code(KC_LALT);
 
-    // 拡大と縮小
-    if (my_abs(x) < my_abs(y)) {
-      register_code(KC_APP);
-
-      if (y < 0) {
-        // swipe up: 拡大
-        tap_code(KC_TAB);
-      } else {
-        // swipe down: 縮小
-        tap_code(KC_MINUS);
-      }
-    }
-
-    // 次のモニターへ移動
-    if (my_abs(x) > my_abs(y)) {
-      register_code(KC_LCTL);
-
-      if (x < 0) {
-        // swipe left:
-        tap_code(KC_APP);
-        tap_code(KC_LEFT);
-      } else {
-        // swipe right:
-        tap_code(KC_APP);
-        tap_code(KC_RIGHT);
-      }
-    }
-    unregister_code(KC_LCMD);
-    unregister_code(KC_LCTL);
-
-    register_code(KC_LALT);
-  }
-
-  // shift
-  if (current_keycode == SFT_T(KC_APP)) {
-    unregister_code(KC_LALT);
-    register_code(KC_LCMD);
-
-    // CMD + J
-    if (my_abs(x) < my_abs(y)) {
-      if (y < 0) {
-        // swipe up:
-        tap_code(KC_J);
-      } else {
-        // swipe down:
-        tap_code(KC_J);
-      }
-    }
-    // ページの戻る、進む
-    if (my_abs(x) > my_abs(y)) {
-      if (x < 0) {
-        // swipe left: 戻る
-        tap_code(KC_LBRC);
-      } else {
-        // swipe right: 進む
-        tap_code(KC_RBRC);
-      }
-    }
-    unregister_code(KC_LCMD);
-    register_code(KC_LALT);
-  }
 
   // CMD
   if (current_keycode == CMD_T(KC_LNG2)) {
